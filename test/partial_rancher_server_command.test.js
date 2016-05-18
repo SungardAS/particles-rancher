d0d631aa3f060d4d2f8fc5d2b6f5964178d8c869
Kevin McGrath <kevin.mcgrath@sungardas.com>
2016-05-18 11:13:57 -0400

    update to condensation 0.5.5 and add tests

---
+++ test/partial_rancher_server_command.test.js
@@ -0,0 +1,32 @@
+var assert = require("assert");
+var CondensationTests = require("condensation-particle-tests");
+
+var cTests = new CondensationTests();
+
+describe("partials", function() {
+  describe("rancher_server_command", function() {
+    it("builds the rancher-server command", function() {
+      cTests.testParticle(
+        "partial",
+        "rancher_server_command",
+        require("./fixtures/partial_rancher_server_command_output_1"),
+        {hArgs: {}}
+      );
+    });
+  });
+
+  it("will add mysql configuration", function() {
+    cTests.testParticle(
+      "partial",
+      "rancher_server_command",
+      require("./fixtures/partial_rancher_server_command_output_2"),
+      {hArgs: {
+        cattleDbCattleMysqlName: "dbName",
+        cattleDbCattleUsername: "username",
+        cattleDbCattlePassword: "password",
+        cattleDbCattleMysqlHost: "localhost",
+        cattleDbCattleMysqlPort: 3306
+      }}
+    );
+  });
+});
