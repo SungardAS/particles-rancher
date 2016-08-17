530afe1ad2219dd07eda6e248697989ce773fd34
Kevin McGrath <kevin.mcgrath@sungardas.com>
2016-08-17 13:14:08 -0400

    fix tests

--- test/partial_userdata_amznlinux.test.js
+++ test/partial_userdata_amznlinux.test.js
@@ -5,12 +5,12 @@ var cTests = new CondensationTests();
 
 describe("partials", function() {
   describe("userdata", function() {
-    describe("amzlinux", function() {
-      it("builds amzlinux cloud-config", function() {
+    describe("amznlinux", function() {
+      it("builds amznlinux cloud-config", function() {
         cTests.testParticle(
           "partial",
-          "userdata/amzlinux",
-          require("./fixtures/partial_userdata_amzlinux_output_1"),
+          "userdata/amznlinux",
+          require("./fixtures/partial_userdata_amznlinux_output_1"),
           {hArgs: {}}
         );
       });
